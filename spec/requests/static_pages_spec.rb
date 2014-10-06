37594f55287ddf2bea9133be466011627a122b47
janusnic <janusnic@gmail.com>
2014-10-06 11:22:46 +0300

    Make a User  model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-  subject { page }
 
   describe "Home page" do
     before { visit root_path }
@@ -8,6 +7,6 @@ require 'spec_helper'
     it { should have_content('Blog App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
-  end
+
 
 end
