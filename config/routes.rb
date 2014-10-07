d30ffe16deb25875859b603dbbb84e95ce7ee07a
janusnic <janusnic@gmail.com>
2014-10-07 13:30:10 +0300

    Finish sibg in

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 BlogApp::Application.routes.draw do
-  #get "static_pages/home"
-  match '/home',   to: 'static_pages#home',   via: 'get'
-  #get "static_pages/about"
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  # get "static_pages/contatct"
-  match '/contact',   to: 'static_pages#contact',   via: 'get'
-  match '/signup',  to: 'users#new',            via: 'get'
-  resources :posts
-
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  root  'static_pages#home'
+  match '/about',  to: 'static_pages#about',            via: 'get'
+  match '/contact',  to: 'static_pages#contact',            via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
+
+  resources :posts
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'index#index'
+#root 'index#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
