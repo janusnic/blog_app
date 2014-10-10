094ad5bd884471b3b7083e9421a44c32d57dcea3
janusnic <janusnic@gmail.com>
2014-10-10 12:31:45 +0300

    Added user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BlogApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   
   root  'static_pages#home'
   match '/about',  to: 'static_pages#about',            via: 'get'
